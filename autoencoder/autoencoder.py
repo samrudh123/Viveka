2cb9c64cb1792e7f6f96d5bbd1abfd446c1fd3c1
rutherston18 <arshmathurj@gmail.com>
2026-01-09 02:36:07 +0530

    writing INN + classifier script (w/o steering)

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,100 +1,157 @@
-# classifier.py
-import torch
+import torch as t
 import torch.nn as nn
-import torch.optim as optim
+import torch.optim as optim 
 from torch.utils.data import DataLoader, TensorDataset
 from torch.utils.tensorboard import SummaryWriter
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import (
-    classification_report, 
-    accuracy_score, 
-    f1_score, 
+    classification_report,
+    accuracy_score,
+    f1_score,
     confusion_matrix
 )
-
 import matplotlib.pyplot as plt
 import seaborn as sns
-import itertools
-import io
 from tqdm import tqdm
 import time
 import numpy as np
 import os
+import glob
+import argparse
+import psutil 
+import wandb
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using device:", device)
+def get_ram_usage_gb():
+    mem = psutil.virtual_memeory()
+    used_gb = mem.used / (1024**3)
+    total_gb =  mem.total / (1024**3)
+    return used_gb, total_gb
 
-class hparams:
-    """Hyperparameters for the 1D Autoencoder"""
-    
-    input_dim = 2304      
-    hidden1 = 256
-    hidden2 = 64
-    latent_dim = 16    
-    
-    # Training parameters
-    batch_size = 256
-    lr = 1e-3
-    num_epochs = 20
-    warmup_steps = 100
-    
-    # Logging
-    logdir = 'runs/autoencoder_1d'
+def get_vram_usage_gb(device=0):
+    used = t.cuda.memory_allocated(device) / (1024**3)
+    reserved = t.cuda.memory_reserved(device) / (1024**3)
+    total = t.cuda.get_device_properties(device).total_memory / (1024**3)
 
-hparams_1 = hparams()
+    return used, reserved, total
 
-# Create log directory if it doesn't exist
-os.makedirs(hparams_1.logdir, exist_ok=True)
+def log_memory(run, step = None):
+    used_ram, total_ram = get_ram_usage_gb()
+    used_vram, reserved_vram, total_vram = get_vram_usage_gb()
 
-class Autoencoder(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-        # Encoder part: compresses the input
-        self.encoder = nn.Sequential(
-            nn.Linear(hparams_1.input_dim, hparams_1.hidden1),
-            nn.ReLU(),
-            nn.Linear(hparams_1.hidden1, hparams_1.hidden2),
-            nn.ReLU(),
-            nn.Linear(hparams_1.hidden2, hparams_1.latent_dim)
-        )
-        
-        # Decoder part: reconstructs from the latent space
-        self.decoder = nn.Sequential(
-            nn.Linear(hparams_1.latent_dim, hparams_1.hidden2),
-            nn.ReLU(),
-            nn.Linear(hparams_1.hidden2, hparams_1.hidden1),
-            nn.ReLU(),
-            nn.Linear(hparams_1.hidden1, hparams_1.input_dim),
-        )
+    run.log({
+        "RAM_used_GB" : used_ram,
+        "VRAM_used_GB" : used_vram,
+        "VRAM_reserved_GB" : reserved_vram,
+    }, step = step)
 
-    def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return decoded
-
-def lr_lambda(current_step):
-    if current_step < hparams_1.warmup_steps:
-        return float(current_step) / float(max(1, hparams_1.warmup_steps))
-    return 1.0
-
-
-def log_confusion_matrix(writer, labels, preds, epoch, class_names=['0', '1']):
+def log_confusion_matrix(writer,labels,preds,epoch,class_names=['0','1']):
     cm = confusion_matrix(labels, preds)
-    fig, ax = plt.subplots(figsize=(4, 4))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=class_names, yticklabels=class_names)
+    fig, ax = plt.subplots(figsize=(4,4))
+    sns.heatmap(cm, annot=True,fmt="d",cmap="Blues",xticklabels=class_names,yticklabels=class_names)
     ax.set_xlabel("Predicted")
     ax.set_ylabel("Actual")
     ax.set_title("Confusion Matrix")
     writer.add_figure("ConfusionMatrix/val", fig, global_step=epoch)
     plt.close(fig)
 
-# TODO : 
-# 1. Add skip connections (make a U-net architechture)
-# 2. Add a classification head on the bottleneck layer
-# 3. Make 2 losses --> One classification loss, and one reconstruction loss
-#    The goal of the algorithm would be to minimise this combined (optionally weighted) loss 4
-# 4. Invertible function instead of a vanilla autoencoder --> the model should be able to reconstruct properly 
 
-# we can try evaluating on other datasets --> this might give us better results than NN Probes
+class EarlyStopping:
+    def __init__(self,patience=5,save_path="checkpoint.pt",delta=0.0):
+        self.patience = patience
+        self.save_path = save_path
+        self.delta = delta
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+
+    def __call__(self, val_loss,model,optimizer,epoch,hparams=None):
+        score = -val_loss
+        if self.best_score is None or score > self.best_score + self.delta:
+            self.best_score = score
+            self.counter = 0
+            t.save ({
+                "epoch" : epoch,
+                "model_state_dict" : model.state_dict(),
+                "optimizer_state_dict" : optimizer.state_dict(),
+                "hparams" : hparams
+            }, self.save_path)
+
+        else : 
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+
+# here I implement the coupling layer from the INN paper as it is
+
+class CouplingLayer(nn.Module):
+
+    def __init__(self,dim,hidden_dim=256,clamp=2.0):
+        super().__init__
+        self.clamp = clamp
+
+        # first split
+
+        self.s1 = nn.Sequential(
+            nn.Linear(dim//2, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, dim // 2)
+        )
+
+        self.t1 = nn.Sequential(
+            nn.Linear(dim // 2, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, dim // 2)
+        )
+        
+        # second split
+        self.s2 = nn.Sequential(
+            nn.Linear(dim // 2, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, dim // 2)
+        )
+        
+        self.t2 = nn.Sequential(
+            nn.Linear(dim // 2, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, dim // 2)
+        )
+
+    def forward(self ,x) : 
+        x1, x2 = t.chunk(x, 2, dim=1)
+
+
+        # clamp is there to control the model output so that it does not explode/vanish since we are 
+        # gonna use exponentials soon
+        s2_out = self.s2(x2).clamp(-self.clamp, self.clamp)
+        y1 = x1 * t.exp(s2_out) + self.t2(x2)
+
+        s1_out = self.s1(y1).clamp(-self.clamp,self.clamp)
+        y2 = x2 * t.exp(s1_out) + self.t1(y1)
+
+        y = t.cat([y1,y2],dim=1)
+        return y
+    
+    def inverse(self,y):
+        y1, y2 = t.chunk(y, 2, dim=1)
+
+        #first inverse transformation
+        s1_out = self.s1(y1).clamp(-self.clamp,self.clamp)
+        x2 = (y2 - self.t1(y1)) * t.exp(-s1_out)
+
+        # second inverse transformation
+
+        s2_out = self.s2(x2).clamp(-self.clamp,self.clamp)
+        x1 = (y1 - self.t2(x2)) * t.exp(-s2_out)
+
+        x = t.cat([x1,x2],dim=1)
+        return x
+    
+
+
